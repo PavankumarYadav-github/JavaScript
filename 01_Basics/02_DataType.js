e355a634e9c848f4e0ac070304e396d935d1f632
PavankumarYadav-github <pavankumar91234@gmail.com>
2023-12-22 17:21:52 +0530

    Datatype JS

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,36 @@
+"Use Strict"; //treat all JS Code as newer version
+
+//alert(30+3); //It will work in brower
+
+let age=25;
+//number 
+//bigint
+//String=""
+//boolean=>true/false
+//null <- standalone value
+//undefine => variable declare but empty value ,value not assign
+//symbol => unique 
+
+//Object
+
+console.log(typeof "hello");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+const array=["","",""];
+
+let obj1={
+    name:"Don",
+    age:23
+}
+
+const fun=function myfun() {
+    console.log("Hello World");
+}
+
+console.log(typeof array);
+console.log(typeof obj1);
+console.log(typeof fun);
