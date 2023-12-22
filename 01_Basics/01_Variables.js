5ddc9283fc270099116667d39296c4409c907299
PavankumarYadav-github <pavankumar91234@gmail.com>
2023-12-22 17:15:54 +0530

    Variable JS

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,30 @@
+
+// dynamically typed language
+// JavaScript is a dynamically typed language, 
+// which means that data types of variables are determined by the value they hold at runtime 
+// and can change throughout the program as we assign different values to them
+
+const accountId = 144241;
+//variable 
+let accountemail = "hello@123";
+var accountPassword = "1245";
+accountcity = "pune";
+let accountstate;
+
+
+
+// accountId = 2; Not Allowed for constant
+
+accountemail = "HW";
+accountPassword = "0000";
+accountcity = "bengaluru";
+
+console.log(accountId);
+console.log(accountemail);
+console.table([accountId,accountemail,accountPassword,accountcity,accountstate]);
+
+/* 
+Prefer not to use var
+because of issue in block scope and function scope 
+*/
+
