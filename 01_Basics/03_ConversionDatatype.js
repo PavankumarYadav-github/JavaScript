4ee9cf67840ec5352524433edf0887180d30ed22
PavankumarYadav-github <pavankumar91234@gmail.com>
2024-03-18 19:50:59 +0530

    Data Type Conversion

---
+++ 01_Basics/03_ConversionDatatype.js
@@ -0,0 +1,116 @@
+// let score="33";
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// console.log("-----------------------------------------");
+
+// let score1="33abc";
+// console.log(typeof score1);
+// console.log(typeof score1);
+// let valueInNumber1=Number(score1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);//NaN Notanumber
+
+// console.log("----------------------------------------");
+
+let score2=null;
+console.log(typeof score2);
+console.log(typeof score2);
+let valueInNumber2=Number(score2);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+// console.log("----------------------------------------");
+
+// let score3=undefined;
+// console.log(typeof score3);
+// console.log(typeof score3);
+// let valueInNumber3=Number(score3);
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);//NaN NotaNumber
+
+// console.log("----------------------------------------");
+
+// let score4=true;
+// console.log(typeof score4);
+// console.log(typeof score4);
+// let valueInNumber4=Number(score4);
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4);//NaN NotaNumber
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+// console.log("_______________________________________________________");
+
+// let isloggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// console.log("_______________________________________________________");
+
+// let isloggedIn1 = "";
+// let booleanIsLoggedIn1 = Boolean(isloggedIn1);
+// console.log(typeof booleanIsLoggedIn1);
+// console.log(booleanIsLoggedIn1);
+
+// console.log("_______________________________________________________");
+
+// let isloggedIn2 = "hello";
+// let booleanIsLoggedIn2 = Boolean(isloggedIn2);
+// console.log(typeof booleanIsLoggedIn2);
+// console.log(booleanIsLoggedIn2);
+
+// 1 => true; 0 => false
+// "" => false
+//"hello" => true
+
+// console.log("####################################");
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+//*************** OPERATION ******************** 
+
+// let value = 3;
+// let negValue=-value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1="hello";
+// let str2=" world";
+// let stt3=str1+str2;
+// console.log(stt3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(true+"");
+// console.log(+"");
+// console.log(""+true);
+
+//Postfix Increment
+let num1=4;
+let num2=num1++;
+console.log(num1+" "+num2);
+//Prefix Increment
+let n1=4;
+let n2=++n1;
+console.log(n1+" "+n2);
